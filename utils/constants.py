9807ce7ac37f18c47822a6de061372dd1dfb0717
superx8ultra <107483876+superx8ultra@users.noreply.github.com>
2022-08-21 11:52:41 +0100

    Add files via upload

--- utils/constants.py
+++ utils/constants.py
@@ -1,13 +1,14 @@
 
 
-BOT_KEY = "NzQwMTg1NTk2NTI1NTQzNDI0.G92hI3.qvqaed90aJW9xKJWx9mwq5dDL5KLxYaKWihGiw"
+BOT_KEY = "OTg2MjQ0MDc5MDUyNTQyMDAy.GaO_V9.OkDx9bMBarnY1iAJEJAnmPQoDGtzRJ8RM2DgRw"
 
 #For Levelling System
-STARTING_VALUE = 800
+STARTING_VALUE = 100
 MULTIPLIER = 1.25
 
 #Aesthetics
 EMBED_COLOUR = 0x036ffc
 
 #Moderation
-STAFF_ROLE = "Staff" #Change this to whatever role you want to have access to moderation commands
+STAFF_ROLE = "Chief Executive Officer"  #Change this to whatever role you want to have access to moderation commands
+ADMIN_ROLE = "Chief Executive Officer", "Chief Operating Officer", "Chief Financial Officer", "Chief Technology Officer", "Chief Marketing Officer", "Chief Security Officer"
